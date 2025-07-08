2a85a87ad9ba552905350dc5c684a9738592e9a2
Thejas <thejasganiga150503@gmail.com>
2025-07-08 23:56:45 +0530

    Sum of Two Numbers

--- Variables_DataTypes/sumOfTwoNumbers.java
+++ Variables_DataTypes/sumOfTwoNumbers.java
@@ -3,10 +3,17 @@
 import java.util.Scanner;
 
 public class sumOfTwoNumbers {
+    public static int sumOfTwo(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number : ");
         int n1 = sc.nextInt();
+        System.out.println("Enter Second Number : ");
         int n2 = sc.nextInt();
+        System.out.println("Sum of 2 Numbers is : " + sumOfTwo(n1, n2));
         sc.close();
     }
 }
