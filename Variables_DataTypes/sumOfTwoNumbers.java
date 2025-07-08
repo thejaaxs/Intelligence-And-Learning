e9e4eec90f2bb5eb63c7d64578a10732d4aa80b3
Thejas <thejasganiga150503@gmail.com>
2025-07-08 23:51:39 +0530

    Sum of Two Numbers

---
+++ Variables_DataTypes/sumOfTwoNumbers.java
@@ -0,0 +1,12 @@
+// Sum of Two Numbers
+
+import java.util.Scanner;
+
+public class sumOfTwoNumbers {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        sc.close();
+    }
+}
