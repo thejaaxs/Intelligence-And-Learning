b15f9856d7da717b993c46ce98cfce7cc1b53e0b
Thejas <thejasganiga150503@gmail.com>
2025-07-09 01:28:51 +0530

    Check Character Type

---
+++ Variables_DataTypes/characterType.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class characterType {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Character : ");
+        char n = sc.next().charAt(0);
+        if (Character.isDigit(n))
+            System.out.println(n + " is a Digit");
+        else if (Character.isAlphabetic(n)) {
+            n = Character.toLowerCase(n);
+            if (n == 'a' || n == 'e' || n == 'i' || n == 'o' || n == 'u')
+                System.out.println(n + " is a Vowel");
+            else
+                System.out.println(n + " is a Consonant");
+        } else
+            System.out.println(n + " Special Character");
+        sc.close();
+    }
+}
