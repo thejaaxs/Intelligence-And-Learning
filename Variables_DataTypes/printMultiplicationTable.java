9e7b6aec43a947206cb715e092eea07ec295e07c
Thejas <thejasganiga150503@gmail.com>
2025-07-09 19:56:55 +0530

    Print Multiplication Number

---
+++ Variables_DataTypes/printMultiplicationTable.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class printMultiplicationTable {
+    static void printMultiplication(int n) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " X " + i + " = " + (n * i));
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+        int n = sc.nextInt();
+        if (n <= 0) {
+            System.out.println("Not Possible");
+            return;
+        }
+        printMultiplication(n);
+        sc.close();
+    }
+}
